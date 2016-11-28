95314081ed8d85b101b22844fe7d6c93f5fb0795
Nestor <agustinduhalde@hotmail.com>
2016-11-28 04:14:31 -0300

    Destrabo camara en el final

--- Juego.cpp
+++ Juego.cpp
@@ -1101,7 +1101,7 @@ public:
 
 		VistaPersonaje* pj = getPersonajeById(id);
 
-		if (pj->getPosCamara() < posCam || pj->getPosCamara() > 539){
+		if (pj->getPosCamara() < posCam){
 			pj->setDerecha(true);
 		}
 		else if (pj->getPosCamara() > posCam){
@@ -1494,7 +1494,7 @@ int main( int argc, char** argv) {
 			auto elapsed_ms = duration_cast<nanoseconds>(deltaTiempo);
 			auto time = elapsed_ms.count()/1000000.0;
 
-			cout << "Elapsed ms: " << time << endl;
+			//cout << "Elapsed ms: " << time << endl;
 
 		}
 	}
